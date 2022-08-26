99a434a389c04bda1429c599c6f8be9cc62fa67f
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2022-08-26 11:36:14 +0800

    Initial Commit

---
+++ Down-Lowd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Down-Lowd
+//
+//  Created by Muhd Syafiq Bin Abas on 26/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Down-Lowd/Down_LowdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Down_LowdApp.swift
+//  Down-Lowd
+//
+//  Created by Muhd Syafiq Bin Abas on 26/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Down_LowdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
