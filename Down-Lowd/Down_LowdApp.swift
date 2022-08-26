bbccf831562d1c5181c620aa4a8398f413709146
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2022-08-26 12:03:05 +0800

    Implemented entry view (splash screen)

--- Down-Lowd/Down_LowdApp.swift
+++ Down-Lowd/Down_LowdApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Down_LowdApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EntryView()
         }
     }
 }
---
+++ Down-Lowd/View/EntryView.swift
@@ -0,0 +1,39 @@
+//
+//  EntryView.swift
+//  Down-Lowd
+//
+//  Created by Muhd Syafiq Bin Abas on 26/8/22.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("SplashImage")
+                    .scaleEffect(0.3)
+                
+                NavigationLink(isActive: self.$isActive) {
+                    ContentView()
+                } label: {
+                    EmptyView()
+                }
+            }
+            .navigationBarHidden(true)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive.toggle()
+                }
+            }
+        }
+    }
+}
+
+struct EntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryView()
+    }
+}
